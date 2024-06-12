270e0861466bb3683cbaf01b0c2d151e2140fc0b
jovrodrigues <joao.rodrigues@philips.com>
2024-06-12 10:58:09 -0300

    Subindo os .gitignore e os paths

--- crud_node_angular/src/app/app-routing.module.ts
+++ crud_node_angular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
+import { ProdutoComponent } from './view/produto/produto.component';
+import { ProdutoGrupoComponent } from './view/produto-grupo/produto-grupo.component';
+import { ProdutoMarcaComponent } from './view/produto-marca/produto-marca.component';
+import { ProdutoSubgrupoComponent } from './view/produto-subgrupo/produto-subgrupo.component';
+import { ProdutoUnidadeComponent } from './view/produto-unidade/produto-unidade.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'produto', component: ProdutoComponent},
+  { path: 'produto-grupo', component: ProdutoGrupoComponent},
+  { path: 'produto-subgrupo', component: ProdutoSubgrupoComponent},
+  { path: 'produto-marca', component: ProdutoMarcaComponent},
+  { path: 'produto-unidade', component: ProdutoUnidadeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
