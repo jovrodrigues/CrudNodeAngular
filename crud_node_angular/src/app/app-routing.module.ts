36be4081c60d3ff52d8504e81a99b4be7077e674
Cristina Siewert Jansen <cristina.jansen@philips.com>
2024-06-12 11:27:07 -0300

    cris_tela-subgrupo

--- crud_node_angular/src/app/app-routing.module.ts
+++ crud_node_angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoSubgrupoComponent } from './view/produto-subgrupo/produto-subgrupo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 'path' : 'subgrupo',
+    'component' : ProdutoSubgrupoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud_node_angular/src/app/app.module.ts
+++ crud_node_angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { ProdutoSubgrupoComponent } from './view/produto-subgrupo/produto-subgrupo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProdutoSubgrupoComponent,
   ],
   imports: [
     BrowserModule,
