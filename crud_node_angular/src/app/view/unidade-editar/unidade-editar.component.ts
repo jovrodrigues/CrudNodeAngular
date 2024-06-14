f2a880aa5f2a4c67a8f342ec36caacfe95b95305
Rafael C. <rafael.carvalho@philips.com>
2024-06-14 14:22:00 -0300

    rafael_tela-produto-inserir

--- crud_node_angular/src/app/view/unidade-editar/unidade-editar.component.ts
+++ crud_node_angular/src/app/view/unidade-editar/unidade-editar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-unidade-editar',
   templateUrl: './unidade-editar.component.html',
-  styleUrls: ['./unidade-editar.component.css']
+  styleUrls: ['../subgrupo-editar/subgrupo-editar.component.css']
 })
 export class UnidadeEditarComponent {
 
