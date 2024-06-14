c19186626edce40153f65ee3721dcbbb6b328df1
tahaskel <thomas.haskel@philips.com>
2024-06-14 13:35:44 -0300

    thomas integracao

--- crud_node_angular/src/app/controller/api.service.ts
+++ crud_node_angular/src/app/controller/api.service.ts
@@ -19,9 +19,8 @@ export class ApiService {
 
   getGrupo(): Observable<Grupo[]> { return this.http.get<Grupo[]>(`${this.apiUrl}/produto_grupo`); }
   postGrupo(data : Grupo): Observable<Grupo> { return this.http.post<Grupo>(`${this.apiUrl}/produto_grupo`, data); }
-
-  //putGrupo(): Observable<Grupo> { return this.http.put<any>(`${this.apiUrl}/produto_grupo/:id`); }
- // deleteGrupo(data : any): Observable<Grupo> { return this.http.delete<any>(`${this.apiUrl}/produto_grupo/:id`, data); }
+  putGrupo(id : any, data: any) { return this.http.put<any>(`${this.apiUrl}/produto_grupo/${id}`, data); }
+  deleteGrupo(id : any) { return this.http.delete(`${this.apiUrl}/produto_grupo/${id}`); }
   
 
   getSubgrupo(): Observable<Subgrupo[]> { return this.http.get<Subgrupo[]>(`${this.apiUrl}/produto_subgrupo`); }
@@ -32,12 +31,18 @@ export class ApiService {
 
   getMarca(): Observable<Marca[]> { return this.http.get<Marca[]>(`${this.apiUrl}/produto_marca`); }
   postMarca(data : Marca): Observable<Marca> { return this.http.post<Marca>(`${this.apiUrl}/produto_marca`, data); }
-  
+  putMarca(id : any, data: any) { return this.http.put<any>(`${this.apiUrl}/produto_marca/${id}`, data); }
+  deleteMarca(id : any) { return this.http.delete(`${this.apiUrl}/produto_marca/${id}`); }
+
 
   getProdutos(): Observable<Produto[]> { return this.http.get<Produto[]>(`${this.apiUrl}/produto`); }
   postProdutos(data : Produto): Observable<Produto> { return this.http.post<Produto>(`${this.apiUrl}/produto`, data); }
+  putProdutos(id : any, data: any) { return this.http.put<any>(`${this.apiUrl}/produto/${id}`, data); }
+  deleteProdutos(id : any) { return this.http.delete(`${this.apiUrl}/produto/${id}`); }
 
 
   getUnidades(): Observable<Unidade[]> { return this.http.get<Unidade[]>(`${this.apiUrl}/produto_unidade`); }
   postUnidades(data : Unidade): Observable<Unidade> { return this.http.post<Unidade>(`${this.apiUrl}/produto_unidade`, data); }
+  putUnidades(id : any, data: any) { return this.http.put<any>(`${this.apiUrl}/produto_unidade/${id}`, data); }
+  deleteUnidades(id : any) { return this.http.delete(`${this.apiUrl}/produto_unidade/${id}`); }
 }
